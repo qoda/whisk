5db01d5a0d362fc4223d19be6a385ca93939162e
Jonathan Bydendyk <jonathan@qoda-macbook.local>
2012-08-03 08:31:22 +0200

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whisk::Application.config.secret_token = '03e22b37485fe5d522226d62a748ae15c10ce832450a8efd06b7ca294516f0d14a86caeed45095f24e2f88539b543172e8d48fa5a36440a0b6fce070259cb9f3'
